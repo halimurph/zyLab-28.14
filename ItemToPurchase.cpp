4cb4faf5fe8162ca4ec6c9ef63241a1d9dae56bc
adhuynh <167336952+adhuynh@users.noreply.github.com>
2024-04-22 13:35:24 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 #include "ItemToPurchase.h"
+
+ItemToPurcahse::ItemToPurchase() { 
+  
+
+}
+
+void ItemToPurchase::SetDescription(string description) { 
+this->description = description;
+}
+
+string ItemToPurchase::GetDescription() {
+return description;
+}
