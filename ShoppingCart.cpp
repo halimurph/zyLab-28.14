63d68136ff0a1502e90645007d321be7c209383a
halimurph <167351997+halimurph@users.noreply.github.com>
2024-04-22 13:25:32 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,18 @@
-/* Type your code here */
+
+
+      ShoppingCart();
+      ShoppingCart(string name, string date);
+      
+      string GetCustomerName() const;
+      string GetDate() const;
+      
+      void AddItem(ItemToPurchase item);
+      void RemoveItem(string name);
+      
+      void ModifyItem(ItemToPurchase item);
+      
+      int GetNumItemsInCart();
+      double GetCostOfCart();
+      
+      void PrintTotal();
+      void PrintDescriptions();
