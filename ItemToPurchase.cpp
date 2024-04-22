b8e1a8b60ec5f5c83d8cef27577cef2a305e7d6d
adhuynh <167336952+adhuynh@users.noreply.github.com>
2024-04-22 13:47:28 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -13,3 +13,28 @@ this->description = description;
 string ItemToPurchase::GetDescription() const {
 return description;
 }
+
+     
+void ItemToPurchase::SetPrice(int price){ 
+this->price = price;
+}
+void ItemToPurchase::SetQuantity(int quantity){ 
+this->quantity = quantity;
+}
+string ItemToPurchase::GetName() const{ 
+return name;
+}
+int ItemToPurchase::GetPrice() const{
+return price;
+}
+int ItemToPurchase::GetQuantity() const {
+return quantity;
+}
+void ItemToPurcahse::PrintItemCost(){
+int totalCost = quantity*price;
+cout << item << quantity << " @ " << price << " = $" << totalCost;
+}
+
+void ItemToPurchase::PrintItemDescription() {
+cout
+}
