2adf1f09d5c6d08f69e378f477d3e2a190381303
halimurph <167351997+halimurph@users.noreply.github.com>
2024-04-22 22:14:45 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -3,20 +3,59 @@ using namespace std;
 
 ShoppingCart::ShoppingCart(){}
       
-ShoppingCart::ShoppingCart(string name, string date){
-      this->
+ShoppingCart::ShoppingCart(string name = "none", string date = "January 1, 2016"){
+      this->customerName = name;
+      this->currentDate = date;
 }
       
-string GetCustomerName() const;
-string GetDate() const;
+string ShoppingCart::GetCustomerName() const{
+      return name;
+}
+
+string ShoppingCart::GetDate() const{
+      return date;
+}
       
-void AddItem(ItemToPurchase item);
-void RemoveItem(string name);
+void ShoppingCart::AddItem(ItemToPurchase item){
+}
+
+void ShoppingCart::RemoveItem(string name){
+      if(name != cartItem.at(i)){
+            cout << "Item not found in cart. Nothing removed." << endl;
+      }else {
+            cartItem.popback.at(i);
+      }
+}
       
-void ModifyItem(ItemToPurchase item);
+void ShoppingCart::ModifyItem(ItemToPurchase item){
+      if(name = cartItem.at(i)){
+            SetDescription(item);
+      }else{
+            cout << "Item not found in cart. Nothing modified." << endl;
+      }
+}
       
-int GetNumItemsInCart();
-double GetCostOfCart();
+int ShoppingCart::GetNumItemsInCart(){
+      int quantity = cartItems.size();
+      returns quantity;
+}
+
+double ShoppingCart::GetCostOfCart(){
+      returns PrintItemCost();
+}
       
-void PrintTotal();
-void PrintDescriptions();
+void ShoppingCart::PrintTotal(){
+      if(cartItems.size() == 0) {
+            cout << "SHOPPING CART IS EMPTY" << endl;
+      }else{
+            cout << totalCost << endl;
+      }
+}
+
+void ShoppingCart::PrintDescriptions(){
+       if(cartItems.size() == 0) {
+            cout << "SHOPPING CART IS EMPTY" << endl;
+} else {
+      cout << PrintItemDescription() << endl;
+       }
+}
