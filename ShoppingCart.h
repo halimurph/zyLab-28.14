a2d96c1659f297cdb09456ec05bacd5a5624193e
halimurph <167351997+halimurph@users.noreply.github.com>
2024-04-28 22:17:45 -0500

    Update ShoppingCart.h

--- ShoppingCart.h
+++ ShoppingCart.h
@@ -11,21 +11,21 @@ class ShoppingCart {
    public:
       ShoppingCart();
       ShoppingCart(string name, string date);
-      
+     
       string GetCustomerName() const;
       string GetDate() const;
-      
+     
       void AddItem(ItemToPurchase item);
       void RemoveItem(string name);
-      
+     
       void ModifyItem(ItemToPurchase item);
-      
+     
       int GetNumItemsInCart();
       double GetCostOfCart();
-      
-      void PrintTotal();
-      void PrintDescriptions();
-      
+     
+      void PrintTotal() ;
+      void PrintDescriptions() ;
+     
    
    private:
       string customerName;
